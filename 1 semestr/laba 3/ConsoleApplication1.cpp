5b73230692b2d86e6eb9dac4dacb9e7400cf1c0b
Alexander <70070040+raik199x@users.noreply.github.com>
2022-03-02 15:48:29 +0000

    Update ConsoleApplication1.cpp

--- 1 semestr/laba 3/ConsoleApplication1.cpp
+++ 1 semestr/laba 3/ConsoleApplication1.cpp
@@ -1,15 +1,14 @@
-﻿// Легкая прога по логике проблем возникнуть не должно.... ведь так?
 #include <stdio.h>
 int main() {
-	int mas[100], n, kolvo = 0,suma = 0; // стандартное объявление (говорящие переменные)
+	int mas[100], n, kolvo = 0,suma = 0;
 	printf("enter amount ");
 	scanf_s("%d", &n); // вводим кол-во элементов
 	for (int i = 0; i < n; i++) {
 		scanf_s("%d", &mas[i]); // заполняем массив
-		if (mas[i] % 5 == 0 && mas[i] % 7 != 0) { // самый сок: делаем проверку делимость на 5 и неделимости на 7
+		if (mas[i] % 5 == 0 && mas[i] % 7 != 0) { //делаем проверку делимость на 5 и неделимости на 7
 			kolvo = kolvo + 1; // в случае успеха увилечиваем количество на 1
 			suma = suma + mas[i]; // и суммируем
 		}
 	}
-	printf("kolichestvo:%d\nsumma:%d", kolvo, suma); // выводим ответ, simple
+	printf("kolichestvo:%d\nsumma:%d", kolvo, suma); // выводим ответ
 }
